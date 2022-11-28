617c125d30d26af40506a510fcfac00ac4952381
Siim Kasela <siim.kasela@aalto.fi>
2022-11-28 16:43:07 +0200

    Fixed minor bug with boss enemy

--- src/runtimeerror.cpp
+++ src/runtimeerror.cpp
@@ -14,8 +14,7 @@ RuntimeError::RuntimeError(RuntimeErrorType subType, QList<QPointF> path, Game& 
 
     case RuntimeErrorType::MemoryStackMinion:
         setPixmap(QPixmap(":/images/Stack_overflow6.png"));
-        setOffset(QPointF(16,16));
-        setTransformOriginPoint(pixmap().height(), pixmap().width());
+        setTransformOriginPoint(pixmap().height()/2, pixmap().width()/2);
         damage_ = 10;
         health_ = 5;
         speed_ = 5;
@@ -35,22 +34,24 @@ void RuntimeError::takeDamage(int damage)
         int stages = 6;
         int increments = pointValue_/stages;
 
-        if((health_-1)/increments > (health_-damage)/increments)
+        if(health_/increments > (health_-damage)/increments )
         {
-            int stage = (health_-damage)/increments;
+            int stage = (health_-damage)/increments-1;
+            if(stage<0) stage = 0;
             QList<QPixmap> stageImgs;
             stageImgs << QPixmap(":/images/Stack_overflow6.png") << QPixmap(":/images/Stack_overflow5.png")
                       << QPixmap(":/images/Stack_overflow4.png") << QPixmap(":/images/Stack_overflow3.png")
-                      << QPixmap(":/images/Stack_overflow2.png") << QPixmap(":/images/Stack_overflow1.png");
+                      << QPixmap(":/images/Stack_overflow2.png") ;
             setPixmap(stageImgs[stage]);
-//                setTransformOriginPoint(pixmap().height()/2, pixmap().width()/2);
+            setTransformOriginPoint(pixmap().height()/2, pixmap().width()/2);
             QList<QPointF> path(path_);
             path.remove(0,point_index_);
             path.push_front(scenePos());
             RuntimeError* minion = new RuntimeError(RuntimeErrorType::MemoryStackMinion,path, game_ );
             minion->setSpeed(speed_*2);
-            minion->startMove();
             game_.addItem(minion);
+            minion->startMove();
+
         }
 
     }
