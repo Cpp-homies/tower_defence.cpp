11c6db101150044a581d58ba1467667be3a58e1f
rv <harvey.lim@aalto.fi>
2022-12-10 16:26:48 +0200

    Enemies breaking comment fixed

--- src/game.cpp
+++ src/game.cpp
@@ -1275,11 +1275,6 @@ bool Game::sellTower(int row, int column) {
         changeCurrency((COM_COST * (1 - SELL_PENALTY)));
         coordsOfTowers.removeOne(QPointF(row, column));
         coordsOfTowers.squeeze();
-        QTimer* timer = new QTimer(this);
-        timer->setSingleShot(true);
-        timer->setInterval(200);
-        timer->callOnTimeout([this](){emit wallAction();});
-        timer->start();
         return true;
     }
 }
@@ -1305,7 +1300,11 @@ void Game::deleteComment(int row, int column) {
     this->mapLayout->removeItem(item);
     this->mapLayout->addItem(path, row, column);
     comment->deleteLater();
-
+    QTimer* timer = new QTimer(this);
+    timer->setSingleShot(true);
+    timer->setInterval(200);
+    timer->callOnTimeout([this](){emit wallAction();});
+    timer->start();
 }
 
 QWidget* Game::getWidgetAt(int row, int column) {
